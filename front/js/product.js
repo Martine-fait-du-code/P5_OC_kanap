df52954500bf1e97e23c23435a0a19e6380c4ffb
Stéphanie Vialardi <s.addams@hotmail.fr>
2022-09-12 18:42:05 +0200

    add page product

---
+++ front/js/product.js
@@ -0,0 +1,15 @@
+// get id for each items
+
+const getArticleId = window.location.search;
+const urlParams = new URLSearchParams(getArticleId);
+const id = urlParams.get("id");
+
+// affichage de l'article séléctionné par l'id
+
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then((products) => products.json())
+  .then((article => displayArticle(article))
+
+
+
+
